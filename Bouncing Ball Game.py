cf311cd0e94373c298ef58a944640184ce9bc9c7
Isaiah <155377636+Isaiahpeoples@users.noreply.github.com>
2024-01-04 06:32:46 -0600

    Add files via upload

---
+++ Bouncing Ball Game.py
@@ -0,0 +1,95 @@
+from tkinter import *
+import random
+import time
+
+class Ball:
+    def __init__(self, canvas, paddle, score, color):
+        self.canvas = canvas
+        self.paddle = paddle
+        self.score = score
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        random_x = random.randrange(0, 485)
+        random_y = random.randrange(0, 200)
+        self.canvas.move(self.id, random_x, random_y)
+        starts = [-3, -2, -1, 1, 2, 3]
+        self.x = random.choice(starts)
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                self.x += self.paddle.x
+                self.score.hit()
+                return True
+        return False
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = self.y * -1
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
+            self.y = self.y * -1
+        if pos[0] <= 0 or pos[2] >= self.canvas_width:
+            self.x = self.x * -1
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.started = False
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        self.canvas.bind_all('<Button-1>', self.start_game)
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0 or pos[2] >= self.canvas_width:
+            self.x = 0
+    def turn_left(self, evt):
+        self.x = -2
+    def turn_right(self, evt):
+        self.x = 2
+    def start_game(self, evt):
+        self.started = True
+
+class Score:
+    def __init__(self, canvas, color):
+        self.score = 0
+        self.canvas = canvas
+        self.id = canvas.create_text(450, 10, text=self.score, fill=color)
+
+    def hit(self):
+        self.score += 1
+        self.canvas.itemconfig(self.id, text=self.score)
+
+tk = Tk()
+tk.title('Bouncing Ball Game')
+tk.resizable(0, 0)
+tk.wm_attributes('-topmost', 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0, background='black')
+canvas.pack()
+tk.update()
+
+score = Score(canvas, 'green')
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, score, 'red')
+game_over_text = canvas.create_text(250, 200, text='GAME OVER', fill='white', font=('Courier',22), state='hidden')
+
+while 1:
+    if ball.hit_bottom == False and paddle.started == True:
+        ball.draw()
+        paddle.draw()
+    if ball.hit_bottom == True:
+        time.sleep(1)
+        canvas.itemconfig(game_over_text, state='normal')
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
